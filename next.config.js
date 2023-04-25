eeb181341aa1b0290e9f40fee373725a088cdcac
panda437 <asifkabeer1@gmail.com>
2023-04-25 22:06:57 +0530

    try2

--- next.config.js
+++ next.config.js
@@ -11,7 +11,7 @@ module.exports = {
   async headers() {
     return [
       {
-        source: '/:path*{/}?',
+        source: '/blog/:path*{/}?',
         headers: [
           {
             key: 'Permissions-Policy',
@@ -20,50 +20,6 @@ module.exports = {
         ]
       }
     ]
-  },
-  async rewrites() {
-    return [
-      {
-        source: '/notes/:pathname',
-        destination: '/api/htmlrewrite?pathname=:pathname'
-      },
-      {
-        source: '/notes/:pathname/b/:slug*',
-        destination: '/api/htmlrewrite?pathname=:pathname&slug=/b/:slug*'
-      },
-      {
-        source: '/notes/:pathname/x/:slug*',
-        destination: '/api/htmlrewrite?pathname=:pathname&slug=/x/:slug*'
-      },
-      {
-        source: '/api/:slug*',
-        destination: 'https://www.craft.do/api/:slug*'
-      },
-      {
-        source: '/share/static/js/:slug*',
-        destination:
-          '/api/jsrewrite?url=https://www.craft.do/share/static/js/:slug*'
-      },
-      {
-        source: '/share/static/css/:slug*',
-        destination: 'https://www.craft.do/share/static/css/:slug*'
-      },
-      {
-        source: '/share/static/fonts/:slug*',
-        destination: 'https://www.craft.do/share/static/fonts/:slug*'
-      },
-      {
-        source: '/share/static/media/:slug*',
-        destination: 'https://www.craft.do/share/static/media/:slug*'
-      },
-      {
-        source: '/share/static/craft.webmanifest',
-        destination: 'https://www.craft.do/share/static/craft.webmanifest'
-      },
-      {
-        source: '/assets/js/analytics2.js',
-        destination: 'https://www.craft.do/404'
-      }
-    ]
   }
+ 
 }
